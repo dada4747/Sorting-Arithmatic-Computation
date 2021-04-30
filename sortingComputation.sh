88d14d7f4d60ba3d07107df86aea9755a496c957
dada4747 <adsurerahul96@gmail.com>
2021-04-30 15:07:19 +0530

    ADDED:Store result in dictionary

--- sortingComputation.sh
+++ sortingComputation.sh
@@ -2,7 +2,19 @@
 read -p "Enter a" a
 read -p "Enter b" b
 read -p "Enter c" c
-res1=$(($a + ($b * $c)))
-res2=$((($a * $b) + $c))
-res3=$(($c + ($a / $b) ))
-res4=$((($a % $b) + $c))
+
+res[1]=$(($a + ($b * $c)))
+res[2]=$((($a * $b) + $c))
+res[3]=$(($c + ($a / $b) ))
+res[4]=$((($a % $b) + $c))
+
+declare -A arr
+
+for((i=1;i<=4;i++))
+do
+	for((j=1;j<=1;j++))
+	do
+		arr["$i"]=${res[$i]}
+	done
+done
+
